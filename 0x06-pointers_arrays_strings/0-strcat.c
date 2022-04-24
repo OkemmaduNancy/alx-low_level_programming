51d0e8f09fd3114659ea10129ba95ce9d1a239cb
OkemmaduNancy <anurika.okemmadu@gmail.com>
2022-04-24 09:23:21 -0700

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+# inclure  " main.h "
+
+/* *
+* _strcat - concatène deux chaînes
+* @dest : chaîne à modifier.
+* @src : chaîne à copier à la fin de la fin.
+* Retour : pointeur vers dest.
+ */
+char * _strcat ( char *dest, char *src)
+{
+	entier je = 0 ;
+
+	for (; *(dest + i) != ' \0 ' ; i++)
+	{
+	}
+	pour (; *src != ' \0 ' ; src++)
+	{
+		*(dest + je) = *src ;
+		je++ ;
+	}
+	*(dest + je) = *src ;
+
+	retour (dest);
+}
