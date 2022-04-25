36d8ec79f5b0ad2106cbecac624abe027e6c4794
OkemmaduNancy <anurika.okemmadu@gmail.com>
2022-04-25 01:55:37 -0700

    Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,19 +6,13 @@
  * @src: string to be copied to the end of end.
  * Return: pointer to dest.
  */
-char *_strncat(char *dest, char *src)
+char *_strncat(char *dest, const char *src, size_t n)
 {
-	int i = 0;
-
-	for (; *(dest + i) != '\0'; i++)
-	{
-	}
-	for (; *src != '\0'; src++)
-	{
-		*(dest + i) = *src;
-		i++;
-	}
-	*(dest + i) = *src;
-
-	return (dest);
+	size_t dest_len = strlen(dest);
+	size_t i;
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[dest_len + i] = src[i];
+	dest[dest_len + i] = '\0';
+	return dest;
 }
+
